c593efd6f31a885a9e57d48462eb31b76fb661f1
Cho Cho Theint <chochotheint19@gmail.com>
2021-10-14 20:40:04 +0630

    first commit

--- homework/hw3/js/app.js
+++
@@ -1,9 +0,0 @@
-const faqtoggles = document.querySelectorAll('.faq-toggle');
-
-faqtoggles.forEach(faqtoggle=>{
-
-    faqtoggle.addEventListener('click',()=>{
-        faqtoggle.parentElement.classList.toggle('active');
-    });
-    
-});
