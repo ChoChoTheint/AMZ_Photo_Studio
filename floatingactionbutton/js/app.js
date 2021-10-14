7c65dddfb322c91cd9d8d990121b8a8e2a100d59
Cho Cho Theint <chochotheint19@gmail.com>
2021-10-14 20:53:46 +0630

    first commit

--- floatingactionbutton/js/app.js
+++
@@ -1,7 +0,0 @@
-// UI
-const btns = document.querySelector('.btns');
-const btnprimary = document.getElementById('btnprimary');
-
-btnprimary.addEventListener('click',()=>{
-    btns.classList.toggle('show');
-})
